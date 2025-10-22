58beb7e0a9e18fc526d10b8cf2081433b28307d3
Jedrzej Drygas <jedrzej@wayfdigital.com>
2025-10-22 18:41:15 +0200

    chore: bump version to 0.1.2-beta, include all Python packages in MANIFEST.in

--- src/wagtail_seotoolkit/__init__.py
+++ src/wagtail_seotoolkit/__init__.py
@@ -2,7 +2,7 @@
 Wagtail SEO Toolkit - A comprehensive SEO package for Wagtail CMS
 """
 
-__version__ = "0.1.1"
+__version__ = "0.1.2-beta"
 
 # Ensure hooks are loaded
 default_app_config = 'wagtail_seotoolkit.apps.WagtailSEOToolkitConfig'
