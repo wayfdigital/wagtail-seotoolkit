8beb76f57c7650f76b2bea3fa3bd9cb04d5642f8
Jedrzej Drygas <jedrzej@wayfdigital.com>
2025-10-23 12:47:01 +0200

    fix: migration dependency we don't need newest only the initial

--- src/wagtail_seotoolkit/migrations/0002_seoauditissue_page.py
+++ src/wagtail_seotoolkit/migrations/0002_seoauditissue_page.py
@@ -7,8 +7,8 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('wagtail_seotoolkit', '0001_initial'),
-        ('wagtailcore', '0095_groupsitepermission'),
+        ("wagtail_seotoolkit", "0001_initial"),
+        ("wagtailcore", "0001_initial"),
     ]
 
     operations = [
